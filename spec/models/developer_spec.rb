471f0d71cfc8519e53c77d4c4c7e6502d7be6dfd
Wes Morgan <wesmorgan@icloud.com>
2018-07-27 16:37:53 -0400

    Add Developer#matched_job spec for no sponsoring employers

--- spec/models/developer_spec.rb
+++ spec/models/developer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Developer do
-  context "#matched_job" do
+  context "#matched_job with sponsoring companies" do
     let!(:local_job) { create :job, :office, latitude: 42, longitude: -78,
                        can_sponsor: true }
     let!(:remote_job) { create :job, :remote, can_sponsor: true }
@@ -17,6 +17,25 @@ describe Developer do
       expect(us_dev.matched_job).to include remote_job
       expect(intl_dev.matched_job).to include local_job
       expect(intl_dev.matched_job).to include remote_job
+    end
+  end
+
+  context "#matched_job with no sponsoring companies" do
+    let!(:local_job) { create :job, :office, latitude: 42, longitude: -78,
+                       can_sponsor: false }
+    let!(:remote_job) { create :job, :remote, can_sponsor: false }
+    let!(:us_dev) { create :developer, :remote_and_office,
+                    need_us_permit: false,
+                    skills_array: ["apache/1", "android/3"] }
+    let!(:intl_dev) { create :developer, :remote_and_office,
+                      need_us_permit: true,
+                      skills_array: ["apache/1", "android/3"] }
+
+    it 'shows both jobs for US dev, neither for intl dev' do
+      expect(us_dev.matched_job).to include local_job
+      expect(us_dev.matched_job).to include remote_job
+      expect(intl_dev.matched_job).not_to include local_job
+      expect(intl_dev.matched_job).not_to include remote_job
     end
   end
 end
