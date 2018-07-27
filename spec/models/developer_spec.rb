e16b22244b2ce76b2d9506ba696dbdaffcdeeb5c
Wes Morgan <wesmorgan@icloud.com>
2018-07-27 17:06:23 -0400

    DRY up developer specs a little

--- spec/models/developer_spec.rb
+++ spec/models/developer_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 describe Developer do
+  let!(:us_dev) { create :developer, :remote_and_office,
+                  need_us_permit: false,
+                  skills_array: ["apache/1", "android/3"] }
+  let!(:intl_dev) { create :developer, :remote_and_office,
+                    need_us_permit: true,
+                    skills_array: ["apache/1", "android/3"] }
+
   context "#matched_job with sponsoring companies" do
     let!(:local_job) { create :job, :office, latitude: 42, longitude: -78,
                        can_sponsor: true }
     let!(:remote_job) { create :job, :remote, can_sponsor: true }
-    let!(:us_dev) { create :developer, :remote_and_office,
-                    need_us_permit: false,
-                    skills_array: ["apache/1", "android/3"] }
-    let!(:intl_dev) { create :developer, :remote_and_office,
-                      need_us_permit: true,
-                      skills_array: ["apache/1", "android/3"] }
 
     it 'shows both jobs for both devs' do
       expect(us_dev.matched_job).to include local_job
@@ -24,12 +25,6 @@ describe Developer do
     let!(:local_job) { create :job, :office, latitude: 42, longitude: -78,
                        can_sponsor: false }
     let!(:remote_job) { create :job, :remote, can_sponsor: false }
-    let!(:us_dev) { create :developer, :remote_and_office,
-                    need_us_permit: false,
-                    skills_array: ["apache/1", "android/3"] }
-    let!(:intl_dev) { create :developer, :remote_and_office,
-                      need_us_permit: true,
-                      skills_array: ["apache/1", "android/3"] }
 
     it 'shows both jobs for US dev, neither for intl dev' do
       expect(us_dev.matched_job).to include local_job
