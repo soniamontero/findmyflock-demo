58f6e1642e12165238a7da2dce0735f8412e8d05
Rebecca Klein <rebecca@radialdevgroup.com>
2018-07-30 15:32:48 -0600

    More tests for matching jobs

--- spec/features/developer_dashboard_spec.rb
+++ spec/features/developer_dashboard_spec.rb
@@ -5,6 +5,7 @@ feature "Developer dashboard" do
 
   let!(:active_company) { create :company, vetted: true }
   let!(:active_job) { create :job, :remote, company_id: active_company.id }
+  let!(:no_benefits_job) { create :job, :remote, company_id: active_company.id, benefits: [] }
 
   let!(:inactive_company) { create :company }
   let!(:inactive_job) { create :job, :remote, company_id: inactive_company.id }
@@ -14,5 +15,12 @@ feature "Developer dashboard" do
   scenario "does not see jobs for inactive companies" do
     expect(page).to have_content active_job.title
     expect(page).to_not have_content inactive_job.title
+  end
+
+  scenario "can filter by benefits", js: true do
+    click_on "Benefits"
+    check "Office Dogs"
+    expect(page).to_not have_content no_benefits_job.title
+    expect(page).to have_content active_job.title
   end
 end
