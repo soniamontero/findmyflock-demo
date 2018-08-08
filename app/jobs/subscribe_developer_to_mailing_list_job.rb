1801916aaa2be84b3dfecf2173ac1641a168ef78
Sonia Montero <sonia.montero@skema.edu>
2018-08-08 13:36:30 +0800

    updated args in job to subscribe developer to mailist

--- app/jobs/subscribe_developer_to_mailing_list_job.rb
+++ app/jobs/subscribe_developer_to_mailing_list_job.rb
@@ -1,8 +1,8 @@
 class SubscribeDeveloperToMailingListJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
-    gibbon.lists.subscribe({:id => ENV["MAILCHIMP_LIST_ID"], :email => {:email => developer.email}, :merge_vars => {:FNAME => developer.first_name, :LNAME => developer.last_name}, :double_optin => false})
+  def perform(developer)
+    gibbon = Gibbon::Request.new(api_key: Rails.application.credentials.mailchimp[:api_key])
+    gibbon.lists.subscribe({:id => Rails.application.credentials.mailchimp[:list_id], :email => {:email => developer.email}, :merge_vars => {:FNAME => developer.first_name, :LNAME => developer.last_name}, :double_optin => false})
   end
 end
