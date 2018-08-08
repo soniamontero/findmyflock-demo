fa243a8524d8010931bd6d8aeca315cdad0ca4fc
Sonia Montero <sonia.montero@skema.edu>
2018-08-08 11:37:41 +0800

    added dotenv

--- app/jobs/subscribe_developer_to_mailing_list_job.rb
+++ app/jobs/subscribe_developer_to_mailing_list_job.rb
@@ -2,6 +2,8 @@ class SubscribeDeveloperToMailingListJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    gb = Gibbon::API.new
+    gb.lists.subscribe({:id => ENV["MAILCHIMP_LIST_ID"], :email => {:email => user.email}, :double_optin => false})
+  end
   end
 end
