ff2121ecba7656e6fb013541089e6b6bea7d1897
Rebecca <rfjackson11@gmail.com>
2018-08-10 11:25:38 -0600

    clean up and rubocop settings

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 class Job < ApplicationRecord
   belongs_to :company
   has_many :matches, dependent: :destroy
   has_many :skills, as: :skillable, dependent: :destroy
   has_many :developers, through: :matches
   has_many :applications, through: :matches
-  validates :title, presence: true,  length: { maximum: 100 }
-  validates :description, presence: true, length: { maximum: 10000 }
-  validates :city, :state, :country, presence: true,  length: { maximum: 100 }
-  validates :max_salary, numericality: { only_integer: true, greater_than: 0}, :allow_nil => true
-  validates :remote, inclusion: { in: [["remote"], ["office"], ["remote", "office"]]}
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :description, presence: true, length: { maximum: 10_000 }
+  validates :city, :state, :country, presence: true, length: { maximum: 100 }
+  validates :max_salary, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
+  validates :remote, inclusion: { in: [['remote'], ['office'], ['remote', 'office']] }
   validates :employment_type, presence: true, length: { maximum: 100 }
   validates :benefits, length: { minimum: 1 }, on: :update
   geocoded_by :location
@@ -18,25 +20,26 @@ class Job < ApplicationRecord
   before_validation :sanitize_benefits_cultures
 
   scope :active, -> { where(active: true, company: Company.active) }
-  scope :check_location, -> (miles, lat, long) {
+  scope :remote_and_local_jobs, ->(miles, lat, long) { where(id: (Job.all_remote.pluck(:id) + Job.check_location(miles, lat, long).pluck(:id)).uniq) }
+  scope :remote_or_office_jobs, ->(array) { where('remote <@ ARRAY[?]::text[] OR remote @> ARRAY[?]::text[]', array, array) }
+  scope :can_sponsor, -> { where(can_sponsor: true) }
+  scope :match_skills_type, ->(array) { where.not(skills_array: []).where('skills_array <@ ARRAY[?]::text[]', array) }
+  scope :filter_by_salary, ->(value) { where('max_salary >= ?', value) }
+  scope :filter_by_benefits, ->(array) { where('benefits @> ARRAY[?]::text[]', array) }
+  scope :filter_by_cultures, ->(array) { where('cultures @> ARRAY[?]::text[]', array) }
+  scope :filter_by_employment_type, ->(value) { where('employment_type = ?', value) }
+  scope :filter_by_city, ->(array) { where(city: array) }
+  scope :order_by_vetted, -> { order(vetted: :desc) }
+
+  scope :all_remote, -> { where("'remote' = ANY (remote)") }
+
+  def self.check_location miles, lat, long
     if lat.present?
       geocoded.near([lat, long], miles, units: :mi).unscope(:order)
     else
       all
     end
-  }
-  scope :remote_and_local_jobs, -> (miles, lat, long) { where(id: (Job.all_remote.pluck(:id) + Job.check_location(miles, lat, long).pluck(:id)).uniq) }
-  scope :remote_or_office_jobs, -> (array) { where("remote <@ ARRAY[?]::text[] OR remote @> ARRAY[?]::text[]", array, array) }
-  scope :can_sponsor, -> { where(can_sponsor: true) }
-  scope :match_skills_type, -> (array) { where.not(skills_array: []).where("skills_array <@ ARRAY[?]::text[]", array) }
-  scope :filter_by_salary, -> (value) {where("max_salary >= ?", value)}
-  scope :filter_by_benefits, -> (array) { where("benefits @> ARRAY[?]::text[]", array) }
-  scope :filter_by_cultures, -> (array) { where("cultures @> ARRAY[?]::text[]", array) }
-  scope :filter_by_employment_type, -> (value) { where("employment_type = ?", value) }
-  scope :filter_by_city, -> (array) { where(city: array)}
-  scope :order_by_vetted, -> { order(vetted: :desc) }
-
-  scope :all_remote, -> { where("'remote' = ANY (remote)") }
+  end
 
   def location
     [city, zip_code, state, country].compact.join(', ')
@@ -52,13 +55,14 @@ class Job < ApplicationRecord
   end
 
   def check_coordinates
-    errors.add(:city, "There is a problem with your location. Please try again") if latitude.nil?
+    location_error = 'There is a problem with your location. Please try again'
+    errors.add(:city, location_error) if latitude.nil?
   end
 
   def toggle_to_vetted
-    if company.vetted?
-      self.vetted = true
-    end
+    self.vetted = true if company.vetted?
   end
 
+  def matched_devs
+  end
 end
