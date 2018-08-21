c3cc5c9c6183cff2885a720bcb13893825060e8b
Rebecca Klein <rebecca@radialdevgroup.com>
2018-08-21 09:52:49 -0600

    move activate to index and make it a button
    - WIP, still need to update CSS

--- spec/features/recruiter_jobs_spec.rb
+++ spec/features/recruiter_jobs_spec.rb
@@ -76,12 +76,12 @@ feature 'Jobs' do
 
       scenario 'can deactivate a job' do
         visit dashboard_companies_path
-        within('div.matched-job', text: job_to_change.title) { click_on 'Edit' }
-
-        uncheck 'Active'
-        click_on 'Continue'
+        within('div.matched-job', text: job_to_change.title) { click_on 'Deactivate' }
 
         expect(job_to_change.reload.active).to eq false
+        within('#nav-profile') {
+          expect(page).to_not have_content job_to_change.title
+        }
       end
     end
   end
@@ -96,26 +96,26 @@ feature 'Jobs' do
       sign_in recruiter
     end
 
-    scenario 'can deactivate a job' do
+    scenario 'can deactivate a job from the index' do
       visit dashboard_companies_path
-      within('div.matched-job', text: job.title) { click_on 'Edit' }
-
-      expect(page).to_not have_content "You are not a member anymore"
-
-      uncheck 'Active'
-      click_on 'Continue'
+      within('div.matched-job', text: job.title) { click_on 'Deactivate' }
 
       expect(job.reload.active).to eq false
+      within('#nav-profile') {
+        expect(page).to_not have_content job.title
+      }
     end
 
     scenario 'inactive jobs do not show by default on index' do
       visit dashboard_companies_path
-      within('.show') do
+      within('#nav-profile') do
         expect(page).to_not have_content inactive_job.title
       end
 
       click_on "Inactive Jobs"
-      expect(page).to have_content inactive_job.title
+      within('#nav-inactive') {
+        expect(page).to have_content inactive_job.title
+      }
     end
   end
 
