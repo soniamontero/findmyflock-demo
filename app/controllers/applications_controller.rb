5c18103786d383849614dcab4ac94951762734b7
Sonia Montero <sonia.montero@skema.edu>
2018-08-29 11:15:58 +0800

    style improvements

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -11,8 +11,9 @@ class ApplicationsController < ApplicationController
   end
 
   def new
-    @application = Application.find_by(match: @match)
-    @application = Application.new if !@application
+    # @application = Application.find_by(match: @match)
+    # @application = Application.new if !@application
+    @application = Application.where(match: @match).first_or_initialize
     @developer = current_developer
     @is_posted = application_is_posted? @match
     @applications_sent = applications_sent_today
