05eddb80c96fce7be200c5ff75416ed14ffad133
Sonia Montero <sonia.montero@skema.edu>
2018-09-11 11:56:53 +0800

    fixed the tests

--- spec/features/developer_delete_account_spec.rb
+++ spec/features/developer_delete_account_spec.rb
@@ -16,10 +16,27 @@ feature 'Deleting developer\'s account' do
     click_on 'Send application'
     clear_emails
   end
-  #   response.should redirect_to '/'
-  #   expect(page).to have_content 'FIND THE COMPANY THAT VALUES THE UNIQUE YOU'
 
-  scenario 'recruiter does not receive email if pending status' do
+  scenario 'recruiter does not receive email if application not opened' do
+    sign_in recruiter
+    click_on 'Applications'
+    within first('.matched-job', text: active_job.title) do
+      expect(page).to have_content "New"
+    end
+
+    click_on "Logout"
+
+    sign_in developer
+    visit edit_developer_registration_path
+    click_on 'Account Settings'
+    click_on 'Delete account'
+    expect(page).to have_content 'Find the company that values the unique you'
+
+    sign_in recruiter
+    expect(emails_sent_to(recruiter.email)).to be_empty
+    end
+
+  scenario 'recruiter receives email if opened status' do
     sign_in recruiter
     click_on 'Applications'
     first('.matched-job', text: active_job.title).click_link('View')
@@ -31,32 +48,10 @@ feature 'Deleting developer\'s account' do
     visit edit_developer_registration_path
     click_on 'Account Settings'
     click_on 'Delete account'
-    Capybara.current_driver = :selenium_chrome
-    # accept_alert do
-    #   click_link('OK')
-    # end
+    expect(page).to have_content 'Find the company that values the unique you'
 
     sign_in recruiter
-    expect(emails_sent_to(recruiter.email)).to be_empty
+    open_email recruiter.email
+    expect(current_email).to have_content 'We’ve deleted their application and information from your dashboard.'
   end
-
-  # scenario 'recruiter receives email if opened status' do
-  #   sign_in recruiter
-  #   click_on 'Applications'
-  #   first('.matched-job', text: active_job.title).click_link('View')
-  #   expect(page).to have_content "Current state\nOpened"
-
-  #   sign_in developer
-  #   visit edit_developer_registration_path
-  #   click_on 'Account Settings'
-  #   click on 'Delete account'
-
-  #   page.accept_confirm_alert 'Are you sure?' do
-  #     click_link 'Ok'
-  #   end
-
-  #   sign_in recruiter
-  #   open_email recruiter.email
-  #   expect(current_email).to have_content 'We’ve deleted their application and information from your dashboard.'
-  # end
 end
