dafa8af3d84bb8c64557cb2a619329725c522a91
Sonia Montero <sonia.montero@skema.edu>
2018-09-12 12:17:27 +0800

    fixed styling issues and tests

--- spec/features/developer_delete_account_spec.rb
+++ spec/features/developer_delete_account_spec.rb
@@ -1,20 +1,18 @@
 require 'rails_helper'
 
 feature 'Deleting developer\'s account' do
-  let(:developer)   { create :developer, :with_profile, :remote }
+  let(:developer) { create :developer, :with_profile, :remote }
   let(:company) { create :company, vetted: true }
 
-  let!(:recruiter)  { create :recruiter, company: company }
+  let!(:recruiter) { create :recruiter, company: company }
   let!(:active_job) { create :job, :remote, company: company }
 
   before do
     sign_in developer
-    clear_emails
 
     within('.matched-job', text: active_job.title) { click_on 'Details' }
     fill_in 'Write a message to the recruiter', with: 'Message'
     click_on 'Send application'
-    clear_emails
   end
 
   scenario 'recruiter does not receive email if application not opened' do
@@ -31,8 +29,6 @@ feature 'Deleting developer\'s account' do
     click_on 'Account Settings'
     click_on 'Delete account'
     expect(page).to have_content 'Find the company that values the unique you'
-
-    sign_in recruiter
     expect(emails_sent_to(recruiter.email)).to be_empty
     end
 
@@ -48,10 +44,10 @@ feature 'Deleting developer\'s account' do
     visit edit_developer_registration_path
     click_on 'Account Settings'
     click_on 'Delete account'
+    expect{click_on 'Delete account'}.to change{Developer.count}.by(-1)
     expect(page).to have_content 'Find the company that values the unique you'
 
-    sign_in recruiter
     open_email recruiter.email
-    expect(current_email).to have_content 'We’ve deleted their application and information from your dashboard.'
+    expect(current_email).to have_content "We’ve deleted their application and information from your dashboard."
   end
 end
