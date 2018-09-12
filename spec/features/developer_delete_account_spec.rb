4b85a6c746cda71b731c907080dcb8162f19ba87
Sonia Montero <sonia.montero@skema.edu>
2018-09-12 13:17:12 +0800

    fixed tests

--- spec/features/developer_delete_account_spec.rb
+++ spec/features/developer_delete_account_spec.rb
@@ -9,10 +9,12 @@ feature 'Deleting developer\'s account' do
 
   before do
     sign_in developer
+    clear_emails
 
     within('.matched-job', text: active_job.title) { click_on 'Details' }
     fill_in 'Write a message to the recruiter', with: 'Message'
     click_on 'Send application'
+    clear_emails
   end
 
   scenario 'recruiter does not receive email if application not opened' do
@@ -29,6 +31,7 @@ feature 'Deleting developer\'s account' do
     click_on 'Account Settings'
     click_on 'Delete account'
     expect(page).to have_content 'Find the company that values the unique you'
+
     expect(emails_sent_to(recruiter.email)).to be_empty
     end
 
@@ -43,10 +46,10 @@ feature 'Deleting developer\'s account' do
     sign_in developer
     visit edit_developer_registration_path
     click_on 'Account Settings'
-    click_on 'Delete account'
     expect{click_on 'Delete account'}.to change{Developer.count}.by(-1)
     expect(page).to have_content 'Find the company that values the unique you'
 
+    sign_in recruiter
     open_email recruiter.email
     expect(current_email).to have_content "We’ve deleted their application and information from your dashboard."
   end
