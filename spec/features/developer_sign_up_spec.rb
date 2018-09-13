ac76ea2c25077356fa6dede6a7d629b0b09d32c8
Sonia Montero <sonia.montero@skema.edu>
2018-09-13 15:11:24 +0800

    fixed the tests

--- spec/features/developer_sign_up_spec.rb
+++ spec/features/developer_sign_up_spec.rb
@@ -26,7 +26,7 @@ feature 'Developer sign up' do
 
     visit root_path
     click_on 'Join'
-    expect(page).to have_content 'Create your developer account'
+    expect(page).to have_content 'Create your job seeker account'
     fill_in 'Email', with: 'mary@exmaple.com'
     fill_in 'Password', with: 'Password1'
     fill_in 'Password confirmation', with: 'Password1'
@@ -42,7 +42,7 @@ feature 'Developer sign up' do
 
     visit root_path
     click_on 'Join'
-    expect(page).to have_content 'Create your developer account'
+    expect(page).to have_content 'Create your job seeker account'
     fill_in 'Email', with: 'mary@exmaple.com'
     fill_in 'Password', with: 'Password1'
     fill_in 'Password confirmation', with: 'Password1'
