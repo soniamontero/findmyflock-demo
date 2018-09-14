288548c3842e140b98a08251b4085c52fdb7a977
Rebecca <rfjackson11@gmail.com>
2018-09-14 08:29:10 -0600

    update status (and delete match if not valid)

--- app/controllers/admin/matches_controller.rb
+++ app/controllers/admin/matches_controller.rb
@@ -7,4 +7,21 @@ class Admin::MatchesController < Admin::BaseController
     @job_title = Job.find(params[:id]).title
     @matches = Match.where(job_id: params[:id])
   end
+
+  def update
+    match = Match.find(params[:id])
+
+    respond_to do |format|
+      if match.update(match_params)
+        format.html { redirect_to request.referrer, notice: 'Match was successfully updated.' }
+      else
+        match.destroy unless match.valid?
+        format.html { redirect_to request.referrer, alert: match.errors.full_messages.to_sentence }
+      end
+    end
+  end
+
+  def match_params
+    params.require(:match).permit(:status)
+  end
 end
