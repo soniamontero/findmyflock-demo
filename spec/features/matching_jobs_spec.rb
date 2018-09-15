ab1d17cd8b9ee0665321ee0080c47e3261eeafa2
Sonia Montero <sonia.montero@skema.edu>
2018-09-15 17:20:00 +0800

    updated view and added tests

--- spec/features/matching_jobs_spec.rb
+++ spec/features/matching_jobs_spec.rb
@@ -143,4 +143,52 @@ feature "Matching Jobs" do
       expect(page).to_not have_content far_away_job.title
     end
   end
+
+  context 'developer notifications' do
+    let!(:developer) { create :developer, :with_profile, :remote }
+
+    before do
+      sign_in developer
+      visit edit_developer_registration_path
+      click_on 'Account settings'
+    end
+
+    scenario 'developer has turned off email notifications' do
+      uncheck 'developer_notifications'
+      click_on 'Save'
+      visit developer_path(developer)
+      expect(page).to have_content "Email notifications: Disabled"
+    end
+
+    scenario 'developer has turned on email notifications' do
+      check 'developer_notifications'
+      click_on 'Save'
+      visit developer_path(developer)
+      expect(page).to have_content "Email notifications: Enabled"
+    end
+  end
+
+  context 'developer matches notifications' do
+    let!(:developer) { create :developer, :with_profile, :remote }
+
+    before do
+      sign_in developer
+      visit edit_developer_registration_path
+      click_on 'Account settings'
+    end
+
+    scenario 'developer has turned off email notifications' do
+      uncheck 'developer_notifications'
+      click_on 'Save'
+      visit developer_path(developer)
+      expect(page).to have_content "Email notifications: Disabled"
+    end
+
+    scenario 'developer has turned on email notifications' do
+      check 'developer_notifications'
+      click_on 'Save'
+      visit developer_path(developer)
+      expect(page).to have_content "Email notifications: Enabled"
+    end
+  end
 end
