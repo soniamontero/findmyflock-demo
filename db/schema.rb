eefd8a5871a614b84acbb71e99a3714066b1ae25
Sonia Montero <sonia.montero@skema.edu>
2018-09-19 10:40:27 +0800

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_05_043753) do
+ActiveRecord::Schema.define(version: 2018_09_15_042926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -124,6 +124,8 @@ ActiveRecord::Schema.define(version: 2018_07_05_043753) do
     t.boolean "first_login", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "gets_mail"
+    t.boolean "notifications", default: true
     t.index ["email"], name: "index_developers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_developers_on_reset_password_token", unique: true
   end
