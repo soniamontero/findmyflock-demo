c71e31cf710ca77ead2f3896ba1a12a3fd4eb443
Rebecca <rfjackson11@gmail.com>
2018-09-19 15:02:46 -0600

    prefix job_title

--- app/models/match.rb
+++ app/models/match.rb
@@ -15,7 +15,8 @@ class Match < ApplicationRecord
 
   delegate :status, to: :application, prefix: true, allow_nil: true
   delegate :full_name, to: :developer
-  delegate :company_name, :company_url, :company_vetted, :title, to: :job
+  delegate :company_name, :company_url, :company_vetted, to: :job
+  delegate :title, to: :job, prefix: true
 
   def match_is_valid?
     if !developer.matched_job.include?(job)
