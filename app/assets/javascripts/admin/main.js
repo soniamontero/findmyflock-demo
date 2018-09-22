a164163ac0b338ad5f27f33834eafb430f61a95f
Rebecca <rfjackson11@gmail.com>
2018-09-22 07:29:30 -0600

    hide save button until changed

--- app/assets/javascripts/admin/main.js
+++ app/assets/javascripts/admin/main.js
@@ -1,8 +1,5 @@
 jQuery(function($) {
   $(".dropdown").change(function() {
-    var rowValue = $(this).closest('.dropdown').find('option:selected').val();
-    console.log('selected', rowValue);
-
-    $(this).closest('tr').addClass('email');
+    $(this).closest('.dropdown').find('input[type=submit]').removeClass('hidden');
   });
 });
