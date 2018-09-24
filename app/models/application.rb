71ddf441b8a0a2dea35e47b68aa10e76df5bc9d0
Sonia Montero <sonia.montero@skema.edu>
2018-09-24 14:45:19 +0800

    update reminder method

--- app/models/application.rb
+++ app/models/application.rb
@@ -14,23 +14,13 @@ class Application < ApplicationRecord
   delegate :recruiters_mail, to: :company, allow_nil: true
 
   def self.reminder
-    applications_array = []
-    byebug
-    new_reminders = 0
     all.where(status: ["pending", "opened"]).each do |app|
-      if ((app.updated_at - app.created_at) / 1.day) >= 7
-        new_reminders =+ 1
-        applications_array << app
-      end
-    end
-    if new_reminders.positive?
-      applications_array.each do |app|
-        if app.last_mail_sent == nil || ((Time.now - app.last_mail_sent) / 1.day) >= 7
-          application_id = app.id
-          CompanyMailer.application_reminder(application_id).deliver
-          app.last_mail_sent == Time.now
-        end
+      if ((Time.now - app.last_mail_sent) / 1.day) >= 7
+        application_id = app.id
+        CompanyMailer.application_reminder(application_id).deliver
+        app.last_mail_sent == Time.now
       end
     end
   end
 end
+
