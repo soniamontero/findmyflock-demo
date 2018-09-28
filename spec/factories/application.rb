97b78fe4c67e4aa77f56e9ee13490a249b717019
Sonia Montero <sonia.montero@skema.edu>
2018-09-28 12:54:02 +0800

    added factory for match and application, and created application model tests

---
+++ spec/factories/application.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :application do
+    message "Hire me please"
+    status 0
+    match
+  end
+end
---
+++ spec/models/application_spec.rb
@@ -0,0 +1,120 @@
+require 'rails_helper'
+
+describe Application do
+  let(:developer) { create :developer, :with_profile, :remote }
+  let(:company) { create :company, vetted: true }
+  let!(:recruiter) { create :recruiter, company: company }
+  let(:job) { create :job, company: company }
+  let(:match) { create :match, job: job, developer: developer}
+
+  context 'last mail is more than 7 days' do
+    context 'status is pending or opened' do
+      let(:pending_application) {
+                                  create :application,
+                                  match: match,
+                                  status: 'pending',
+                                  last_mail_sent: 10.days.ago,
+                                  created_at: 20.days.ago,
+                                  updated_at: 12.days.ago
+                                }
+      let(:opened_application) {
+                                 create :application,
+                                 match: match,
+                                 status: 'opened',
+                                 last_mail_sent: 10.days.ago,
+                                 created_at: 20.days.ago,
+                                 updated_at: 12.days.ago
+                               }
+
+      it 'sends a reminder to the recruiter' do
+        expect(CompanyMailer).to receive(:application_reminder)
+                             .with([pending_application.id, opened_application.id])
+                             .and_call_original
+        expect(Application.reminder).to include company
+        Application.reminder
+      end
+    end
+
+    context 'status is contacted or rejected' do
+      let(:contacted_application) {
+                                    create :application,
+                                    match: match,
+                                    status: 'contacted',
+                                    last_mail_sent: 10.days.ago,
+                                    created_at: 20.days.ago,
+                                    updated_at: 12.days.ago
+                                  }
+      let(:rejected_application) {
+                                   create :application,
+                                   match: match,
+                                   status: 'rejected',
+                                   last_mail_sent: 10.days.ago,
+                                   created_at: 20.days.ago,
+                                   updated_at: 12.days.ago
+                                 }
+
+      it 'does not send a reminder to the recruiter' do
+        expect(CompanyMailer).to_not receive(:application_reminder)
+                             .with([contacted_application.id, rejected_application.id])
+                             .and_call_original
+        expect(Application.reminder).to include company
+        Application.reminder
+      end
+    end
+  end
+
+  context 'last mail is less than 7 days' do
+    context 'application is contacted or rejected' do
+      let(:contacted_application) {
+                                    create :application,
+                                    match: match,
+                                    status: 'contacted',
+                                    last_mail_sent: 2.days.ago,
+                                    created_at: 5.days.ago,
+                                    updated_at: 4.days.ago
+                                  }
+      let(:rejected_application) {
+                                   create :application,
+                                   match: match,
+                                   status: 'rejected',
+                                   last_mail_sent: 2.days.ago,
+                                   created_at: 5.days.ago,
+                                   updated_at: 4.days.ago
+                                 }
+
+      it 'does not send a reminder to the recruiter' do
+        expect(CompanyMailer).to_not receive(:application_reminder)
+                             .with([contacted_application.id, rejected_application.id])
+                             .and_call_original
+        expect(Application.reminder).to include company
+        Application.reminder
+      end
+    end
+
+    context 'application is pending or opened' do
+      let(:pending_application) { create :application,
+                                  match: match,
+                                  status: 'pending',
+                                  last_mail_sent: 2.days.ago,
+                                  created_at: 5.days.ago,
+                                  updated_at: 4.days.ago
+                                }
+      let(:opened_application) {
+                                 create :application,
+                                 match: match,
+                                 status: 'pending',
+                                 last_mail_sent: 2.days.ago,
+                                 created_at: 5.days.ago,
+                                 updated_at: 4.days.ago
+                               }
+
+      it 'does not send a reminder to the recruiter' do
+        expect(CompanyMailer).to_not receive(:application_reminder)
+                             .with([pending_application.id, opened_application.id])
+                             .and_call_original
+        expect(Application.reminder).to include company
+        Application.reminder
+      end
+    end
+  end
+end
